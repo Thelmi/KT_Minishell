fe527489c3897bf226bba17137681649ff98b9e9
kamilla-ai <107996427+kamilla-ai@users.noreply.github.com>
2024-09-17 19:13:57 +0400

    Update pwd.c

--- builtins/pwd.c
+++ builtins/pwd.c
@@ -3,21 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   pwd.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: thelmy <thelmy@student.42.fr>              +#+  +:+       +#+        */
+/*   By: krazikho <krazikho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/19 14:14:05 by krazikho          #+#    #+#             */
-/*   Updated: 2024/09/11 04:03:19 by thelmy           ###   ########.fr       */
+/*   Updated: 2024/09/17 17:34:54 by krazikho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
-void pwd(int *last_exit_status)
+
+void	pwd(int *last_exit_status)
 {
-    char cwd[1024];
-    if(!getcwd(cwd, sizeof(cwd))){
-        perror("pwd");
-        *last_exit_status = 1;
-    }else{
-        printf("%s\n", getcwd(cwd, sizeof(cwd)));
-    }
+	char	cwd[124];
+
+	if (!getcwd(cwd, sizeof(cwd)))
+	{
+		perror("pwd");
+		*last_exit_status = 1;
+	}
+	else
+	{
+		printf("%s\n", getcwd(cwd, sizeof(cwd)));
+	}
 }
