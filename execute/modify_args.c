1aefcec51c14dcb849b97f0c563d218c3be287f8
kamilla-ai <107996427+kamilla-ai@users.noreply.github.com>
2024-09-18 13:29:05 +0400

    Update modify_args.c less than 25

--- execute/modify_args.c
+++ execute/modify_args.c
@@ -1,66 +1,136 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   modify_utils.c                                     :+:      :+:    :+:   */
+/*   modify_args.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: krazikho <krazikho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/17 21:08:46 by krazikho          #+#    #+#             */
-/*   Updated: 2024/09/17 21:18:53 by krazikho         ###   ########.fr       */
+/*   Created: 2024/08/27 19:04:39 by krazikho          #+#    #+#             */
+/*   Updated: 2024/09/17 21:18:16 by krazikho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-int	handle_exit_status_len(int *last_exit_status)
+static int	new_len(char *arg, t_env *envir, int *last_exit_status)
 {
-	return (ft_strlen(ft_itoa(*last_exit_status)));
-}
+	int		len;
+	int		i;
 
-int	handle_var_len(char *arg, int *i, t_env *envir)
-{
-	int		var_len;
-	char	var_name[256];
-
-	var_len = 0;
-	(*i)++;
-	while (arg[*i] && (ft_isalnum(arg[*i]) || arg[*i] == '_'))
+	len = 0;
+	i = 0;
+	while (arg[i])
 	{
-		var_name[var_len++] = arg[*i];
-		(*i)++;
+		if (arg[i] == '$' && arg[i + 1] == '?')
+		{
+			len += handle_exit_status_len(last_exit_status);
+			i += 2;
+		}
+		else if (arg[i] == '$' && ft_isalnum(arg[i + 1]))
+		{
+			len += handle_var_len(arg, &i, envir);
+		}
+		else
+		{
+			len++;
+			i++;
+		}
 	}
-	var_name[var_len] = '\0';
-	if (getcopyenv(var_name, &envir))
-		return (ft_strlen(getcopyenv(var_name, &envir)));
-	return (0);
+	return (len);
 }
 
-
-int handle_exit_status(char *res, int j, int *last_exit_status)
+char	*allocate_result(char *arg, t_env *envir, int *last_exit_status)
 {
-	(void)j;
-	ft_strcat(res, ft_itoa(*last_exit_status));
-	return (ft_strlen(res));
+	char	*res;
+
+	res = malloc(sizeof(char) * (new_len(arg, envir, last_exit_status) + 1));
+	if (!res)
+		return (NULL);
+	return (res);
 }
 
-int	handle_var_expansion(char *res, char *arg, int *i, t_env *envir)
+static char	*no_quotes(char *arg, t_env *envir,int *last_exit_status)
 {
-	int		var_len;
+	int		i;
 	int		j;
-	char	var_name[256];
+	char	*res;
 
-	var_len = 0;
-	j = ft_strlen(res);
-	(*i)++;
-	while (arg[*i] && (ft_isalnum(arg[*i]) || arg[*i] == '_'))
+	i = 0;
+	j = 0;
+	res = allocate_result(arg, envir, last_exit_status);
+	if (!res)
+		return (NULL);
+	while (arg[i])
 	{
-		var_name[var_len++] = arg[(*i)++];
+		if (arg[i] == '$' && arg[i + 1] == '?')
+		{
+			j = handle_exit_status(res, j, last_exit_status);
+			i += 2;
+		}
+		else if (arg[i] == '$' && ft_isalnum(arg[i + 1]))
+		{
+			j = handle_var_expansion(res, arg, &i, envir);
+		}
+		else
+			res[j++] = arg[i++];
 	}
-	var_name[var_len] = '\0';
-	if (getcopyenv(var_name, &envir))
+	res[j] = '\0';
+	return (res);
+}
+
+static char	*double_quotes(char *arg, t_env *envir,
+		int *last_exit_status)
+{
+	char	*res;
+	int		len;
+
+	len = ft_strlen(arg);
+	arg[len - 1] = '\0';
+	arg = arg + 1;
+	res = no_quotes(arg, envir, last_exit_status);
+	return (res);
+}
+
+static char	*single_quotes(char *arg)
+{
+	int		i;
+	int		len;
+	char	*res;
+
+	i = 0;
+	len = ft_strlen(arg);
+	res = malloc(sizeof(char) * (len + 1 - 2));
+	if (!res)
+		return (NULL);
+	while (i < len - 2)
 	{
-		ft_strcat(res, getcopyenv(var_name, &envir));
-		j = ft_strlen(res);
+		res[i] = arg[i + 1];
+		i++;
 	}
-	return (j);
+	res[i] = '\0';
+	return (res);
+}
+
+void	modify_args(char **args, t_env *envir, int *last_exit_status)
+{
+	int		i;
+	char	*tmp;
+
+	i = 1;
+	while (args[i])
+	{
+		if (args[i][0] == '\'' && args[i][ft_strlen(args[i]) - 1] == '\'')
+			tmp = single_quotes(args[i]);
+		else if (args[i][0] == '"' && args[i][ft_strlen(args[i]) - 1] == '"')
+		{
+			tmp = double_quotes(args[i], envir,
+					last_exit_status);
+		}
+		else
+			tmp = no_quotes(args[i], envir, last_exit_status);
+		if (args[i])
+			free(args[i]);
+		args[i] = tmp;
+		i++;
+	}
 }
