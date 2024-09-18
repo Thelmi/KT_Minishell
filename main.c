5ad782cf0c60e7fbaa5dba0e6f46bf55400846b5
kamilla-ai <107996427+kamilla-ai@users.noreply.github.com>
2024-09-18 16:06:33 +0400

    Update main.c

--- main.c
+++ main.c
@@ -3,42 +3,59 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mrhelmy <mrhelmy@student.42.fr>            +#+  +:+       +#+        */
+/*   By: krazikho <krazikho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/14 14:58:22 by krazikho          #+#    #+#             */
-/*   Updated: 2024/09/15 22:40:09 by mrhelmy          ###   ########.fr       */
+/*   Updated: 2024/09/18 15:52:49 by krazikho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int main(int ac, char **av, char **ev){
-    (void)ac;
-    (void)av;
-    char *command;
-    t_env *envir;
-	t_export *exp;
-    int last_exit_status;
-    configure_terminal_behavior();
-    signal(SIGINT, sigint_handler);
-    signal(SIGQUIT, sigquit_handler);
-	last_exit_status = 0; 
-    envir = storing_env(ev);
-	exp = storing_export(ev);
-    while(1)
-    {
-        command = readline("minishell$ ");
-        if (command == NULL) {
-		write(1, "exit\n", 5);
-		break;
-        }
-        if (*command) {
-            add_history(command);
-	    runcmd(parsecmd(command, &last_exit_status), ev, &envir, &exp, &last_exit_status);
-        }
-        free(command);
-    }
-    free_env(envir);
-    free_export(exp);
-    return (last_exit_status);
+static void	initialize_shell(char **ev, t_env **envir, t_export **exp,
+		t_context *context)
+{
+	context->last_exit_status = 0;
+	configure_terminal_behavior();
+	setup_signals(context);
+	*envir = storing_env(ev);
+	*exp = storing_export(ev);
+}
+
+static void	command_loop(char **ev, t_env **envir, t_export **exp,
+		t_context *context)
+{
+	char	*command;
+
+	while (1)
+	{
+		command = readline("minishell$ ");
+		if (command == NULL)
+		{
+			write(1, "exit\n", 5);
+			break ;
+		}
+		if (*command)
+		{
+			add_history(command);
+			runcmd(parsecmd(command, &context->last_exit_status), ev, envir,
+				exp, &context->last_exit_status);
+		}
+		free(command);
+	}
+	free_env(*envir);
+	free_export(*exp);
+}
+
+int	main(int ac, char **av, char **ev)
+{
+	t_env		*envir;
+	t_export	*exp;
+	t_context	context;
+
+	(void)ac;
+	(void)av;
+	initialize_shell(ev, &envir, &exp, &context);
+	command_loop(ev, &envir, &exp, &context);
+	return (context.last_exit_status);
 }
