cd9599a2b0c7f7b41d3e792ef9d12c323d5ca569
kamilla-ai <107996427+kamilla-ai@users.noreply.github.com>
2024-09-22 19:06:44 +0400

    Update modify_utils.c

--- utils/modify_utils.c
+++ utils/modify_utils.c
@@ -12,89 +12,89 @@
 
 #include "../minishell.h"
 
-static int	new_len(char *arg, t_env *envir, int *last_exit_status)
-{
-	int	len;
-	int	i;
+// static int	new_len(char *arg, t_env *envir, int *last_exit_status)
+// {
+// 	int	len;
+// 	int	i;
 
-	len = 0;
-	i = 0;
-	while (arg[i])
-	{
-		if (arg[i] == '$' && arg[i + 1] == '?')
-		{
-			len += ft_strlen(ft_itoa(*last_exit_status));
-			i += 2;
-		}
-		else if (arg[i] == '$' && ft_isalnum(arg[i + 1]))
-		{
-			len += handle_var_len(arg, &i, envir);
-		}
-		else
-		{
-			len++;
-			i++;
-		}
-	}
-	return (len);
-}
+// 	len = 0;
+// 	i = 0;
+// 	while (arg[i])
+// 	{
+// 		if (arg[i] == '$' && arg[i + 1] == '?')
+// 		{
+// 			len += ft_strlen(ft_itoa(*last_exit_status));
+// 			i += 2;
+// 		}
+// 		else if (arg[i] == '$' && ft_isalnum(arg[i + 1]))
+// 		{
+// 			len += handle_var_len(arg, &i, envir);
+// 		}
+// 		else
+// 		{
+// 			len++;
+// 			i++;
+// 		}
+// 	}
+// 	return (len);
+// }
 
-char	*allocate_result(char *arg, t_env *envir, int *last_exit_status)
-{
-	char	*res;
+// char	*allocate_result(char *arg, t_env *envir, int *last_exit_status)
+// {
+// 	char	*res;
 
-	res = malloc(sizeof(char) * (new_len(arg, envir, last_exit_status) + 1));
-	if (!res)
-	{
-		perror("malloc");
-		return (NULL);
-	}
-	return (res);
-}
+// 	res = malloc(sizeof(char) * (new_len(arg, envir, last_exit_status) + 1));
+// 	if (!res)
+// 	{
+// 		perror("malloc");
+// 		return (NULL);
+// 	}
+// 	return (res);
+// }
 
-int	handle_var_len(char *arg, int *i, t_env *envir)
-{
-	int		var_len;
-	char	var_name[256];
+// int	handle_var_len(char *arg, int *i, t_env *envir)
+// {
+// 	int		var_len;
+// 	char	var_name[256];
 
-	var_len = 0;
-	(*i)++;
-	while (arg[*i] && (ft_isalnum(arg[*i]) || arg[*i] == '_'))
-	{
-		var_name[var_len++] = arg[*i];
-		(*i)++;
-	}
-	var_name[var_len] = '\0';
-	if (getcopyenv(var_name, &envir))
-		return (ft_strlen(getcopyenv(var_name, &envir)));
-	return (0);
-}
+// 	var_len = 0;
+// 	(*i)++;
+// 	while (arg[*i] && (ft_isalnum(arg[*i]) || arg[*i] == '_'))
+// 	{
+// 		var_name[var_len++] = arg[*i];
+// 		(*i)++;
+// 	}
+// 	var_name[var_len] = '\0';
+// 	if (getcopyenv(var_name, &envir))
+// 		return (ft_strlen(getcopyenv(var_name, &envir)));
+// 	return (0);
+// }
 
-int	handle_exit_status(char *res, int j, int *last_exit_status)
-{
-	(void)j;
-	ft_strcat(res, ft_itoa(*last_exit_status));
-	return (ft_strlen(res));
-}
+// int	handle_exit_status(char *res, int j, int *last_exit_status)
+// {
+// 	(void)j;
+// 	ft_strcat(res, ft_itoa(*last_exit_status));
+// 	return (ft_strlen(res));
+// }
 
-int	handle_var_expansion(char *res, char *arg, int *i, t_env *envir)
-{
-	int		var_len;
-	int		j;
-	char	var_name[256];
+// int	handle_var_expansion(char *res, char *arg, int *i, t_env *envir)
+// {
+// 	int		var_len;
+// 	int		j;
+// 	char	var_name[256];
 
-	var_len = 0;
-	j = ft_strlen(res);
-	(*i)++;
-	while (arg[*i] && (ft_isalnum(arg[*i]) || arg[*i] == '_'))
-	{
-		var_name[var_len++] = arg[(*i)++];
-	}
-	var_name[var_len] = '\0';
-	if (getcopyenv(var_name, &envir))
-	{
-		ft_strcat(res, getcopyenv(var_name, &envir));
-		j = ft_strlen(res);
-	}
-	return (j);
-}
+// 	var_len = 0;
+// 	j = ft_strlen(res);
+// 	(*i)++;
+// 	while (arg[*i] && (ft_isalnum(arg[*i]) || arg[*i] == '_'))
+// 	{
+// 		var_name[var_len++] = arg[(*i)++];
+// 	}
+// 	var_name[var_len] = '\0';
+// 	if (getcopyenv(var_name, &envir))
+// 	{
+// 		ft_strcat(res, getcopyenv(var_name, &envir));
+// 		j = ft_strlen(res);
+// 	}
+// 	return (j);
+// }
